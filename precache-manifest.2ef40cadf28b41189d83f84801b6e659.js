af3c00efbd19f3f2e338ca0a74cd94647e5a7062
dmb-2222 <dmb-2222@yandex.ru>
2019-11-05 19:12:10 +0200

    Updates

---
+++ precache-manifest.2ef40cadf28b41189d83f84801b6e659.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f443681ed698c4fd81fdb0a402e27110",
+    "url": "/goit-react14-hw-04-pet-store/index.html"
+  },
+  {
+    "revision": "a5e5339e4c21924fd8bc",
+    "url": "/goit-react14-hw-04-pet-store/static/css/main.4e61dbd5.chunk.css"
+  },
+  {
+    "revision": "9bcd97707b5c05e53606",
+    "url": "/goit-react14-hw-04-pet-store/static/js/2.624dfb29.chunk.js"
+  },
+  {
+    "revision": "a5e5339e4c21924fd8bc",
+    "url": "/goit-react14-hw-04-pet-store/static/js/main.6d803b2c.chunk.js"
+  },
+  {
+    "revision": "c26368668ab43b2d1630",
+    "url": "/goit-react14-hw-04-pet-store/static/js/runtime-main.05eaf4b9.js"
+  }
+]);
